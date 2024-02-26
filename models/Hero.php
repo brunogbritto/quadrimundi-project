0dc2c05e47798eb17e23d4236ec945c8be8d7eae
brunogbritto <brunoogbritto@gmail.com>
2024-02-26 17:02:05 -0300

    fix: fix changes

--- models/Hero.php
+++ models/Hero.php
@@ -8,28 +8,28 @@ class Hero {
 
     // Método para buscar todos os heróis
     public function getHeroes() {
-        $query = "SELECT * FROM heroes";
+        $query = "SELECT * FROM characters";
         $stmt = $this->conn->query($query);
         return $stmt->fetchAll();
     }
 
     // Método para buscar um herói específico pelo ID
     public function getHeroById($id) {
-        $stmt = $this->conn->prepare("SELECT * FROM heroes WHERE id = ?");
+        $stmt = $this->conn->prepare("SELECT * FROM characters WHERE id = ?");
         $stmt->execute([$id]);
         return $stmt->fetch();
     }
 
     // Método para buscar heroi pelo codAutor
     public function getHeroesByCodAutor($codAutor) {
-        $stmt = $this->conn->prepare("SELECT * FROM heroes WHERE codAutor = ?");
+        $stmt = $this->conn->prepare("SELECT * FROM characters WHERE codAutor = ?");
         $stmt->execute([$codAutor]);
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
     // Método para adicionar um novo herói
     public function addHero($name, $power, $codAutor) {
-        $query = "INSERT INTO heroes (name, power, codAutor) VALUES (?, ?, ?)";
+        $query = "INSERT INTO characters (name, power, codAutor) VALUES (?, ?, ?)";
         $stmt = $this->conn->prepare($query);
         $stmt->execute([$name, $power, $codAutor]);
         return $stmt;
@@ -37,7 +37,7 @@ class Hero {
 
     // Método para atualizar um herói
     public function updateHero($id, $name, $power) {
-        $query = "UPDATE heroes SET name = ?, power = ? WHERE id = ?";
+        $query = "UPDATE characters SET name = ?, power = ? WHERE id = ?";
         $stmt = $this->conn->prepare($query);
         return $stmt->execute([$name, $power, $id]);
          
@@ -45,7 +45,7 @@ class Hero {
 
     // Método para excluir um herói
     public function deleteHero($id) {
-        $stmt = $this->conn->prepare("DELETE FROM heroes WHERE id = ?");
+        $stmt = $this->conn->prepare("DELETE FROM characters WHERE id = ?");
         $stmt->execute([$id]);
         return $stmt;
     }
